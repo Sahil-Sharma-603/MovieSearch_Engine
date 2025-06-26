64a4a7534d1dd1b9cccdb3f524c87a30d9bc8838
Sahil-Sharma-603 <sharmasahil8342@gmail.com>
2025-06-25 22:47:31 -0500

    HomePage search bar state is set and updated

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import MovieCard from './components/moviesCard'
+import Home from "./pages/Home"
 
 function App() {
- 
+
+
   return (
     <>
-    <MovieCard movie = {{title: "Inception", year:"2010"}}/>
-    <MovieCard movie = {{title: "Inception2", year:"2012"}}/>
-    <MovieCard movie = {{title: "Inception3", year:"2015"}}/>
+  
+      <Home />
 
     </>
   )
---
+++ src/pages/Home.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+import MovieCard from "../components/moviesCard";
+
+function Home(){
+    // State to hold the search query
+  const [searchQuery, setSearchQuery] = useState("");
+
+   const  movies = [
+        {id: 1, title: "Inception", year: 2010},
+        {id: 2, title: "Interstellar", year: 2014},
+        {id: 3, title: "Dunkirk", year: 2017},
+    ]
+
+    const handleSearch =()=>{
+            alert(searchQuery);
+    };
+
+    return(
+        <div className = "home">
+
+            <form onSubmit = {handleSearch} className = "search-form"> 
+                <input type = "text" 
+                placeholder="Search Movies ...."
+                 className="search-input"
+                 value = {searchQuery} 
+                 onChange = {(e) => setSearchQuery(e.target.value)}
+                />        
+                <button type = "submit" className= "search-button" >Search</button>
+            </form>
+
+            <div className="movies-grid">
+                
+                {movies.map((movie) => (
+                    <MovieCard movie = {movie} key = {movie.id}/>
+                ))}
+            </div>
+
+        </div>
+    );
+}
+
+
+export default Home;
